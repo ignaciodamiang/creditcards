ff6d8981d3ad250c8cf64fbc36adae8683607a99
Ignacio Damián González <ignaciodamiang@gmail.com>
2023-03-05 08:29:34 -0300

    CreditCardController + tests

---
+++ src/main/java/com/nacho/creditcards/controllers/CreditCardController.java
@@ -0,0 +1,56 @@
+package com.nacho.creditcards.controllers;
+
+import com.nacho.creditcards.entities.CreditCard;
+import com.nacho.creditcards.services.interfaces.ICreditCardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/credit-cards")
+public class CreditCardController {
+
+    @Autowired ICreditCardService creditCardService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CreditCard> getCreditCardById(@PathVariable Long id) {
+        CreditCard creditCard = creditCardService.getCreditCardById(id);
+        if (creditCard == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(creditCard);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CreditCard>> getAllCreditCards() {
+        List<CreditCard> creditCards = creditCardService.getAllCreditCards();
+        if (creditCards.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(creditCards);
+    }
+
+    @PostMapping
+    public ResponseEntity<CreditCard> createCreditCard(@RequestBody CreditCard creditCard) {
+        CreditCard createdCreditCard = creditCardService.createCreditCard(creditCard);
+        return new ResponseEntity<>(createdCreditCard, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CreditCard> updateCreditCard(@PathVariable Long id, @RequestBody CreditCard creditCard) {
+        CreditCard updatedCreditCard = creditCardService.updateCreditCard(id, creditCard);
+        if (updatedCreditCard == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(updatedCreditCard);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCreditCard(@PathVariable Long id) {
+        creditCardService.deleteCreditCard(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/test/java/com/nacho/creditcards/controllers/CreditCardControllerTest.java
@@ -0,0 +1,165 @@
+package com.nacho.creditcards.controllers;
+
+import com.nacho.creditcards.entities.CardBrand;
+import com.nacho.creditcards.entities.CreditCard;
+import com.nacho.creditcards.exceptions.CreditCardNotValidException;
+import com.nacho.creditcards.services.interfaces.ICreditCardService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.YearMonth;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class CreditCardControllerTest {
+
+    @Mock
+    private ICreditCardService creditCardService;
+
+    @Test
+    public void testGetCreditCardById() {
+        // Arrange
+        CreditCard creditCard = CreditCard.builder()
+                .id(1L)
+                .cardNumber("1234 5678 9012 3456")
+                .holderName("John Doe")
+                .expirationDate(YearMonth.of(2025, 12))
+                .brand(CardBrand.VISA)
+                .build();
+        when(creditCardService.getCreditCardById(creditCard.getId())).thenReturn(creditCard);
+
+        CreditCardController controller = new CreditCardController();
+        controller.creditCardService = creditCardService;
+
+        // Act
+        ResponseEntity<CreditCard> response = controller.getCreditCardById(creditCard.getId());
+
+        // Assert
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo(creditCard);
+    }
+    
+    @Test
+    public void testGetAllCreditCards() {
+        // Arrange
+        List<CreditCard> creditCards = Arrays.asList(
+            CreditCard.builder()
+                .id(1L)
+                .cardNumber("1234 5678 9012 3456")
+                .holderName("John Doe")
+                .expirationDate(YearMonth.of(2025, 12))
+                .brand(CardBrand.VISA)
+                .build(),
+            CreditCard.builder()
+                .id(2L)
+                .cardNumber("5678 1234 9012 3456")
+                .holderName("Jane Doe")
+                .expirationDate(YearMonth.of(2023, 9))
+                .brand(CardBrand.VISA)
+                .build()
+        );
+        when(creditCardService.getAllCreditCards()).thenReturn(creditCards);
+
+        CreditCardController controller = new CreditCardController();
+        controller.creditCardService = creditCardService;
+
+        // Act
+        ResponseEntity<List<CreditCard>> response = controller.getAllCreditCards();
+
+        // Assert
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo(creditCards);
+    }
+
+    @Test
+    public void testCreateCreditCard() throws CreditCardNotValidException {
+        // Arrange
+        CreditCard creditCard = CreditCard.builder()
+                .id(1L)
+                .cardNumber("1234 5678 9012 3456")
+                .holderName("John Doe")
+                .expirationDate(YearMonth.of(2025, 12))
+                .brand(CardBrand.VISA)
+                .build();
+        
+        CreditCard createdCreditCard = CreditCard.builder()
+                .id(1L)
+                .cardNumber("1234 5678 9012 3456")
+                .holderName("John Doe")
+                .expirationDate(YearMonth.of(2025, 12))
+                .brand(CardBrand.VISA)
+                .build();
+        
+        when(creditCardService.createCreditCard(creditCard)).thenReturn(createdCreditCard);
+        
+        CreditCardController controller = new CreditCardController();
+        controller.creditCardService = creditCardService;
+
+        // Act
+        ResponseEntity<CreditCard> response = controller.createCreditCard(creditCard);
+
+        // Assert
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(response.getBody()).isEqualTo(createdCreditCard);
+    }
+
+    @Test
+    public void testUpdateCreditCard() {
+        // Arrange
+        Long id = 1L;
+        CreditCard creditCardToUpdate = CreditCard.builder()
+                .id(id)
+                .cardNumber("1234 5678 9012 3456")
+                .holderName("John Doe")
+                .expirationDate(YearMonth.of(2025, 12))
+                .brand(CardBrand.VISA)
+                .build();
+        CreditCard updatedCreditCard = CreditCard.builder()
+                .id(id)
+                .cardNumber("1234 5678 9012 3456")
+                .holderName("Jane Doe")
+                .expirationDate(YearMonth.of(2026, 1))
+                .brand(CardBrand.AMEX)
+                .build();
+
+        when(creditCardService.updateCreditCard(id, creditCardToUpdate)).thenReturn(updatedCreditCard);
+
+        CreditCardController controller = new CreditCardController();
+        controller.creditCardService = creditCardService;
+
+        // Act
+        ResponseEntity<CreditCard> response = controller.updateCreditCard(id, creditCardToUpdate);
+
+        // Assert
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo(updatedCreditCard);
+    }
+    
+    @Test
+    public void testDeleteCreditCard() throws Exception {
+        // Arrange
+        Long id = 1L;
+        doNothing().when(creditCardService).deleteCreditCard(id);
+
+        CreditCardController controller = new CreditCardController();
+        controller.creditCardService = creditCardService;
+
+        // Act
+        ResponseEntity<Void> response = controller.deleteCreditCard(id);
+
+        // Assert
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+        verify(creditCardService, times(1)).deleteCreditCard(id);
+    }
+}
